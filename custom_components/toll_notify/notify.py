329fb09f2d75c629a648a44a1274f6a483cb285c
pongdevelop <jinda789amulet@gmail.com>
2021-02-25 23:34:57 +0700

    Update notify.py

--- custom_components/toll_notify/notify.py
+++ custom_components/toll_notify/notify.py
@@ -49,6 +49,6 @@ class LineNotificationService(BaseNotificationService):
        
         r=requests.Session().post(BASE_URL, headers=headers, files=None, data=payload)
         if r.status_code  != 200:
-            _LOGGER.error(r.text)
+            _LOGGER.error(r.content)
 
         _LOGGER.debug(r.content)
