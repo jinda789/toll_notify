bee3e6a30239f5ee34a4d4fbd7e906afc915b8c7
pongdevelop <jinda789amulet@gmail.com>
2021-03-23 12:46:16 +0700

    fix bug filedata

--- custom_components/toll_notify/notify.py
+++ custom_components/toll_notify/notify.py
@@ -48,6 +48,6 @@ class LineNotificationService(BaseNotificationService):
                     'message':message , 'tagkey':self.access_token
                 }) 
        
-        r=requests.Session().post(BASE_URL, headers=headers, files=file, data=payload)
+        r=requests.Session().post(BASE_URL, headers=headers, data=payload)
         if r.status_code  != 200:
             _LOGGER.error(r.text)
