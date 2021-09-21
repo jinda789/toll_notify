415e5a86e9d70e6c1771739ce24c3fdbdced2318
pongdevelop <jinda789amulet@gmail.com>
2021-09-21 16:20:52 +0700

    clean code mawin

--- custom_components/toll_notify/notify.py
+++ custom_components/toll_notify/notify.py
@@ -11,43 +11,41 @@ With this custom component loaded, you can send messaged to tollfree notify.
 
 import requests
 import logging
-import voluptuous as vol
-import json
- 
+
+
 from aiohttp.hdrs import AUTHORIZATION
-import homeassistant.helpers.config_validation as cv
-from homeassistant.const import CONF_ACCESS_TOKEN 
+
+from homeassistant.const import CONF_ACCESS_TOKEN
 from homeassistant.components.notify import (
-    ATTR_DATA, PLATFORM_SCHEMA, BaseNotificationService)
+    ATTR_DATA,
+    PLATFORM_SCHEMA,
+    BaseNotificationService,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
-BASE_URL = 'https://homeassistant.systems/push/toll'
+BASE_URL = "https://homeassistant.systems/push/toll"
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_ACCESS_TOKEN): cv.string,
-})
 
 def get_service(hass, config, discovery_info=None):
     """Get the tollfree notification service."""
-    access_token = config.get(CONF_ACCESS_TOKEN )
+    access_token = config.get(CONF_ACCESS_TOKEN)
     return TollFreeNotificationService(access_token)
-                                           
+
+
 class TollFreeNotificationService(BaseNotificationService):
     """Implementation of a notification service for the tollfree Messaging service."""
 
     def __init__(self, access_token):
         """Initialize the service."""
-        self.access_token = access_token                                           
-        
-    def send_message(self, message="", **kwargs):
-        """Send some message.""" 
-        headers = {AUTHORIZATION:"Bearer "+ self.access_token}
+        self.access_token = access_token
 
-        payload = ({
-                    'message':message , 'tagkey':self.access_token
-                }) 
-       
-        r=requests.Session().post(BASE_URL, headers=headers, data=payload)
-        if r.status_code  != 200:
+    def send_message(self, message="", **kwargs):
+        """Send some message."""
+        headers = {AUTHORIZATION: "Bearer " + self.access_token}
+
+        payload = {"message": message, "tagkey": self.access_token}
+
+        r = requests.Session().post(BASE_URL, headers=headers, data=payload)
+        if r.status_code != 200:
             _LOGGER.error(r.text)
